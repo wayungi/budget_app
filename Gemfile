f2d2a6d98f9a8c67a8d0f43ede60f4777506388a
wayungi <fwayungi@gmail.com>
2022-11-21 17:21:50 +0300

    ADDS: devise and its configurations

--- Gemfile
+++ Gemfile
@@ -51,6 +51,9 @@ gem 'bootsnap', require: false
 # linter
 gem 'rubocop', '>= 1.0', '< 2.0'
 
+# devise for authentication
+gem 'devise', '~> 4.8', '>= 4.8.1'
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[mri mingw x64_mingw]
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
-    validates :name, presence: true, length: { maximum: 20, minimum:1 }
-    has_many :expenses, foreign_key: 'author_id'
-    has_many :categories
+  # Include default devise modules. Others available are:
+  # , :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
+
+  validates :name, presence: true, length: { maximum: 20, minimum:1 }
+  has_many :expenses, foreign_key: 'author_id'
+  has_many :categories
 end
