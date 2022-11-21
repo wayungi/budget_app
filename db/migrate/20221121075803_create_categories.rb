2faf15079bd646f4f0f12ed3c09f96e5c28678b7
wayungi <fwayungi@gmail.com>
2022-11-21 12:02:36 +0300

    UPDATE: foreign key relationships

--- db/migrate/20221121075803_create_categories.rb
+++ db/migrate/20221121075803_create_categories.rb
@@ -3,7 +3,7 @@ class CreateCategories < ActiveRecord::Migration[7.0]
     create_table :categories do |t|
       t.string :name
       t.string :icon
-      t.references :author, null: false, foreign_key: true
+      t.references :author, null: false, foreign_key: {to_table: :users}
       t.string :users
 
       t.timestamps
--- db/migrate/20221121080319_create_expenses.rb
+++ db/migrate/20221121080319_create_expenses.rb
@@ -3,7 +3,7 @@ class CreateExpenses < ActiveRecord::Migration[7.0]
     create_table :expenses do |t|
       t.string :name
       t.decimal :amount
-      t.references :author, null: false, foreign_key: true
+      t.references :author, null: false, foreign_key: {to_table: :users}
 
       t.timestamps
     end
