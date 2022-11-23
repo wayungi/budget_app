c53bb986caaef599a89bbe1e8860ceb2cdb70f33
wayungi <fwayungi@gmail.com>
2022-11-23 19:23:50 +0300

    ADDS: redirection to categories on login

--- app/controllers/landings_controller.rb
+++ app/controllers/landings_controller.rb
@@ -1,3 +1,7 @@
 class LandingsController < ApplicationController
-  def index; end
+  def index
+    redirect_to categories_path if user_signed_in?
+  end
 end
+
+# redirect_to groups_path if user_signed_in?
