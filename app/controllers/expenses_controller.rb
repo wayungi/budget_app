6a95b0d10071142860b43c1dad669f648a4fa213
wayungi <fwayungi@gmail.com>
2022-11-23 19:12:14 +0300

    ADDS: rediction to login for all non authenticated users

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,4 +1,5 @@
 class ExpensesController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_expense, only: %i[show edit update destroy]
   before_action :set_category
 
