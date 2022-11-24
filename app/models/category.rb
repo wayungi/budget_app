47fb4d9772497eb33015b76c8bf19d635e402a1d
wayungi <fwayungi@gmail.com>
2022-11-24 18:03:45 +0300

    ADDS:Rspec setup

--- app/models/category.rb
+++ app/models/category.rb
@@ -5,13 +5,4 @@ class Category < ApplicationRecord
   belongs_to :user, foreign_key: 'author_id'
   has_and_belongs_to_many :expenses
 
-  # def total_exp
-  #   total = 0
-  #   @cats = expenses
-  #   @cats.each do |cat|
-  #     total += cat.amount
-  #   end
-  #   total
-  # end
-
   end
--- spec/model/category_spec.rb
+++ spec/model/category_spec.rb
@@ -2,5 +2,4 @@ require 'rails_helper'
 
 RSpec.describe Category, type: :model do
     user = User.create(name:'francis')
-
 end
