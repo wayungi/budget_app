5edd7fa2a81eec89c0b5e69c56eefb9284afe107
wayungi <fwayungi@gmail.com>
2023-08-12 13:43:48 +0300

    UPDATE: confrimation email sending to user email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,7 +53,9 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   
   # letter opener 
-  config.action_mailer.delivery_method = :letter_opener
+  #! config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :smtp
+
   config.action_mailer.perform_deliveries = true
 
 
@@ -83,4 +85,20 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  #! Enable email sending through gmail
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'render.com',
+    user_name:            'fwayungi@gmail.com',
+    password:             'fmurwpptymbabnzm',
+    authentication:       'plain',
+    enable_starttls_auto: true,
+    open_timeout:         5,
+    read_timeout:         5 
+  }
+  
 end
