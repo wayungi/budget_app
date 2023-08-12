1bdaa23b46f7b4c4ce8e13a86aea755b26dda4f5
wayungi <fwayungi@gmail.com>
2023-08-12 14:13:18 +0300

    Fix linter errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,6 +42,7 @@ Rails.application.configure do
 
   #! Enable email sending through gmail
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
@@ -83,7 +84,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
